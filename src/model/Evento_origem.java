7aa0e7fbf959989abed4ffb8ac723845185d6527
Felipe Roscoche <fer8@pitt.edu>
2016-09-18 18:38:07 -0300

    Inicialização do Projeto SGACERVO

---
+++ src/model/Evento_origem.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Evento_origem {
+     private String nome_evento_origem;
+    private int cod_evento_origem;
+
+    public String getNome_evento_origem() {
+        return nome_evento_origem;
+    }
+
+    public void setNome_evento_origem(String nome_evento_origem) {
+        this.nome_evento_origem = nome_evento_origem;
+    }
+
+    public int getCod_evento_origem() {
+        return cod_evento_origem;
+    }
+
+    public void setCod_evento_origem(int cod_evento_origem) {
+        this.cod_evento_origem = cod_evento_origem;
+    }
+}
